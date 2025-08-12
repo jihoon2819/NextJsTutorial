049508f353e0d802a77c6feafb9adf3cb6a443fd
jihoon2819 <85489123+jihoon2819@users.noreply.github.com>
2025-08-13 02:07:20 +0900

    day4 react js method

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,11 +1,27 @@
-export const metadata = {
-  title: "Home",
-};
+"use client"
 
+import { useEffect, useState } from "react";
+
+// export const metadata = {
+//   title: "Home",
+// };
+//https://nomad-movies.nomadcoders.workers.dev/movies
 export default function Tomato() {
+  const [isLoading,setIsLoading]=useState(true);
+  const [movies,setMovies]=useState([]);
+  const getMovies=async()=>{
+    const response= await fetch('https://nomad-movies.nomadcoders.workers.dev/movies')
+    const json = await response.json();
+    setMovies(json)
+    setIsLoading(false)
+  }
+  useEffect(()=>{
+    getMovies();
+  },[])
   return (
     <div>
       <h1>Hello</h1>
+      {isLoading?"로딩중":JSON.stringify(movies)}
     </div>
   );
 }
