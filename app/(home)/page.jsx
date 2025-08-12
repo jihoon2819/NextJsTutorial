d1d68fcf5e386799a4fd6122f39cbb8d5b841859
jihoon2819 <85489123+jihoon2819@users.noreply.github.com>
2025-08-13 03:08:25 +0900

    Next js fetching

--- app/(home)/page.jsx
+++ app/(home)/page.jsx
@@ -1,27 +1,25 @@
-"use client"
 
-import { useEffect, useState } from "react";
+const URL='https://nomad-movies.nomadcoders.workers.dev/movies'
 
+export const metadata ={
+  title : "home"
+}
+
+async function getMovies(){
+  await new Promise((resolve)=>setTimeout(resolve,5000))
+  const response = await fetch(URL)
+  const json = await response.json()
+  return json
+}
 // export const metadata = {
 //   title: "Home",
 // };
-//https://nomad-movies.nomadcoders.workers.dev/movies
-export default function Tomato() {
-  const [isLoading,setIsLoading]=useState(true);
-  const [movies,setMovies]=useState([]);
-  const getMovies=async()=>{
-    const response= await fetch('https://nomad-movies.nomadcoders.workers.dev/movies')
-    const json = await response.json();
-    setMovies(json)
-    setIsLoading(false)
-  }
-  useEffect(()=>{
-    getMovies();
-  },[])
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
       <h1>Hello</h1>
-      {isLoading?"로딩중":JSON.stringify(movies)}
+      {JSON.stringify(movies)}
     </div>
   );
 }
