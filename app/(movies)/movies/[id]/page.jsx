0c1d62a81cb59ddc3ce555f280e88bfd22c5ace3
jihoon2819 <85489123+jihoon2819@users.noreply.github.com>
2025-08-14 03:45:49 +0900

    day6 suspense and errorhandle

--- app/(movies)/movies/[id]/page.jsx
+++ app/(movies)/movies/[id]/page.jsx
@@ -1,24 +1,15 @@
-import { API_URL } from "@/app/(home)/page";
+import MovieInfo from "@/components/movie-info";
+import MovieVideos from "@/components/movie-videos";
+import { Suspense } from "react";
 
-async function getMovie(id){
-  console.log("제목 요청 시도:", id, new Date().toISOString());
-  const response = await fetch(`${API_URL}/${id}`,{cache:'force-cache'})
-  return response.json();
-}
-async function getVideos(id){
-  console.log("비디오 요청 시도:", id, new Date().toISOString());
-  const response = await fetch(`${API_URL}/${id}/videos`,{cache:'force-cache'})
-  return response.json();
-}
+
 export default async function MoviesDetail({ params, searchParams }) {
   const { id } = await params;
   const { samsung } = await searchParams;
-  console.log(id + " " + samsung);
-  console.log('fetching start')
-  const [movie,videos]= await Promise.all([getMovie(id), getVideos(id)])
-  console.log('end start')
 
   return <div><h1>Mobe {id}</h1>
-  {movie.title}
-  {console.log(videos)}</div>;
+  <Suspense fallback={<h1>Loading info</h1>}><MovieInfo id={id}/></Suspense>
+  <Suspense fallback={<h1>Loading video</h1>}><MovieVideos id={id} /></Suspense>
+  
+  </div>;
 }
